d53ccc4de2c0c46138742cf2c02847de5b3835c6
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-08-26 20:14:24 +0900

    Add examples-scala/basics/request

--- examples/scala/app/controllers/Basics.scala
+++ examples/scala/app/controllers/Basics.scala
@@ -7,14 +7,14 @@ object Basics extends Controller {
   // The charset is handled automatically via the play.api.mvc.Codec type class.
   // Just import an implicit instance of play.api.mvc.Codec in the current scope
   // to change the charset that will be used by all operations.
-  implicit val charset = Codec.javaSupported("utf-8") 
+  implicit val charset = Codec.javaSupported("utf-8")
 
-  def todo = TODO 
+  def todo = TODO
 
   def forbidden = Action {
     // Status(FORBIDDEN)("...")
     Forbidden("Error 403 Forbidden")
-  } 
+  }
 
   def notfound = Action {
     // Status(NOT_FOUND)("...")
@@ -27,7 +27,7 @@ object Basics extends Controller {
     //    SEE_OTHER,
     //    Map(LOCATION -> routes.Basics.plain().toString)),
     //    Enumerator("")
-    //) 
+    //)
     val result = Redirect(routes.Basics.plain())
     println(result)
     result
@@ -36,23 +36,27 @@ object Basics extends Controller {
   def plain = Action {
     // "text/plain" as default for a String value.
     Ok("<pre>This is a text/plain response.</pre>")
-  } 
+  }
 
   def xml = Action {
-    // "text/xml" as default for a XML literal. 
+    // "text/xml" as default for a XML literal.
     var result = Ok(<response>This is a text/xml response.</response>)
     println(result)
     result
-  } 
+  }
 
   def html = Action {
     // Set "text/html" manually by using play.api.mvc.Result#as
     var result = Ok("<strong>This is a text/html response. 日本語表示</strong>").as(HTML)
     println(result)
     result
-  } 
+  }
 
   def application = Action {
     Ok(views.html.basics.application(Play.current))
-  } 
+  }
+
+  def request = Action { request =>
+    Ok(views.html.basics.request(request))
+  }
 }
