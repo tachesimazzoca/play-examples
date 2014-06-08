530dd314437990c6e7721f23634c7e1b1d0772e5
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2014-06-09 05:56:49 +0900

    Update play to v2.3.0 on play-overview

--- scala/overview/project/Build.scala
+++ scala/overview/project/Build.scala
@@ -1,8 +1,11 @@
 import sbt._
 import Keys._
-import play.Project._
+import play.Play.autoImport._
+import PlayKeys._
 
 object ApplicationBuild extends Build {
+
+  scalaVersion := "2.10.4"
 
   val appName         = "play-overview"
   val appVersion      = "1.0-SNAPSHOT"
@@ -11,7 +14,8 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+    version := appVersion,
+    libraryDependencies ++= appDependencies
   )
 }
