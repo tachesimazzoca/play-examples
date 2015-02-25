657659074f9f86c34bdd62b3e09bd97f3120afb1
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-02-25 13:55:51 +0900

    Rename FormSuite with MappingSuite
    - Add more tests

--- scala/overview/test/test/MappingSuite.scala
+++ scala/overview/test/test/MappingSuite.scala
@@ -9,20 +9,23 @@ import play.api.data.format.Formats._
 import play.api.data.validation.Constraints._
 
 @RunWith(classOf[JUnitRunner])
-class FormSuite extends FunSuite {
+class MappingSuite extends FunSuite {
 
-  case class User(name: String, age: Option[Int])
+  case class User(name: String, age: Option[Int], activated: Boolean)
 
   case class Item(id: Option[Long], code: String, name: String, price: Int)
+
+  case class Blog(id: Option[Long], title: String, body: String, publishedAt: Long, tags: List[String])
 
   case class Address(street: String, city: String)
 
   case class Profile(name: String, address: Address)
 
-  test("bind User") {
+  test("User") {
     val m: Mapping[User] = mapping(
       "name" -> of[String],
-      "age" -> optional(of[Int])
+      "age" -> optional(of[Int]),
+      "activated" -> default(of[Boolean], true)
     )(User.apply)(User.unapply)
 
     m.bind(Map.empty) match {
@@ -36,9 +39,10 @@ class FormSuite extends FunSuite {
     val u: User = m.bind(Map("name" -> "Foo", "age" -> "25")).right.get
     assert("Foo" === u.name)
     assert(Some(25) === u.age)
+    assert(true === u.activated)
   }
 
-  test("bind Item") {
+  test("Item") {
     val m: Mapping[Item] = mapping(
       "id" -> optional(longNumber),
       "code" -> text.verifying(pattern( """[A-Z][0-9]{5}""".r)),
@@ -77,19 +81,62 @@ class FormSuite extends FunSuite {
     }
   }
 
+  test("Blog") {
+    val m = mapping(
+      "id" -> optional(longNumber),
+      "title" -> text,
+      "meta" -> tuple(
+        "body" -> text,
+        "publishedAt" -> longNumber
+      ),
+      "tags" -> list(text)
+    ) { (id, title, meta, tags) =>
+      Blog(id, title, meta._1, meta._2, tags)
+    } { (blog) =>
+      Some((blog.id, blog.title, (blog.body, blog.publishedAt), blog.tags))
+    }
+
+    val obj = Blog(
+      Some(12345L),
+      "Play Framework",
+      "I'm binding into Blog!",
+      23456L,
+      List("repeated", "tags")
+    )
+    val params = Map(
+      "id" -> obj.id.get.toString,
+      "title" -> obj.title,
+      "meta.body" -> obj.body,
+      "meta.publishedAt" -> obj.publishedAt.toString,
+      "tags[0]" -> obj.tags(0),
+      "tags[1]" -> obj.tags(1)
+    )
+
+    m.bind(params) match {
+      case Right(blog) =>
+        assert(obj === blog)
+      case Left(_) => fail("It must not fail with a valid map.")
+    }
+    assert(params === m.unbind(obj))
+  }
+
   test("curried apply/unapply") {
-    val params = Map("name" -> "foo", "age" -> "15")
-    val m = mapping[User, String, Option[Int]](
+    val params = Map("name" -> "foo", "age" -> "15", "activated" -> "false")
+    val m = mapping[User, String, Option[Int], Boolean](
       "name" -> of[String],
-      "age" -> optional(of[Int])) _
+      "age" -> optional(of[Int]),
+      "activated" -> default(of[Boolean], true)) _
 
-    val f = m(_: (String, Option[Int]) => User)(User.unapply)
-    assert(Right(User("foo!", Some(15))) === f((name, age) => User(name + "!", age)).bind(params))
-    assert(Right(User("foo", Some(25))) === f((name, age) => User(name, age.map(_ + 10))).bind(params))
+    val f = m(_: (String, Option[Int], Boolean) => User)(User.unapply)
+    assert(Right(User("foo!", Some(15), false)) ===
+      f((name, age, activated) => User(name + "!", age, activated)).bind(params))
+    assert(Right(User("foo", Some(25), false)) ===
+      f((name, age, activated) => User(name, age.map(_ + 10), activated)).bind(params))
 
-    val g = m(User.apply)(_: (User) => Option[(String, Option[Int])])
-    assert(params === g((user) => Some((user.name, user.age.map(_ - 10)))).unbind(User("foo", Some(25))))
-    assert(Map.empty[String, String] === g((user) => None).unbind(User("foo", Some(15))))
+    val g = m(User.apply)(_: (User) => Option[(String, Option[Int], Boolean)])
+    assert(params === g((user) => Some((user.name, user.age.map(_ - 10), user.activated)))
+      .unbind(User("foo", Some(25), false)))
+    assert(Map.empty[String, String] === g((user) => None).unbind(User("foo", Some(15), false)))
   }
 
   test("single") {
@@ -128,12 +175,12 @@ class FormSuite extends FunSuite {
     )(Profile.apply)(Profile.unapply)
 
     m.bind(Map(
-      "name" -> "Nested Tuple",
+      "name" -> "Nested Value",
       "address.street" -> "1-2-3",
       "address.city" -> "Fukuoka"
     )) match {
       case Right(a) =>
-        assert("Nested Tuple" === a.name)
+        assert("Nested Value" === a.name)
         assert("1-2-3" === a.address.street)
         assert("Fukuoka" === a.address.city)
       case Left(_) => fail("It must not fail with a valid map.")
