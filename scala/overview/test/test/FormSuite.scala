bb9f090e61071e1a22ab9a606a09e1e812a72cb1
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-04-19 22:13:54 +0900

    Add test Field

--- scala/overview/test/test/FormSuite.scala
+++ scala/overview/test/test/FormSuite.scala
@@ -9,7 +9,12 @@ import play.api.data.validation.Constraints._
 
 @RunWith(classOf[JUnitRunner])
 class FormSuite extends FunSuite {
+
   case class User(name: String, age: Int)
+
+  case class Item(name: String, variation: Variation, tags: List[String])
+
+  case class Variation(name: String, price: Int)
 
   test("bind") {
     val userForm = Form(mapping(
@@ -29,5 +34,31 @@ class FormSuite extends FunSuite {
         fail("It must fail with an empty map.")
       }
     )
+  }
+
+  test("Field") {
+    val itemForm = Form(mapping(
+      "name" -> nonEmptyText,
+      "variation" -> mapping(
+        "name" -> nonEmptyText,
+        "price" -> number
+      )(Variation.apply)(Variation.unapply),
+      "tags" -> list(text)
+    )(Item.apply)(Item.unapply)).bind(
+        Map(
+          "name" -> "Play Pants",
+          "variation.name" -> "L",
+          "variation.price" -> "1350",
+          "tags[0]" -> "foo",
+          "tags[1]" -> "bar",
+          "tags[2]" -> "baz"
+        )
+      )
+
+    assert("variation_name" === itemForm("variation.name").id)
+    assert("variation.name" === itemForm("variation.name").label)
+    assert("tags_0" === itemForm("tags[0]").id)
+    assert("tags.0" === itemForm("tags[0]").label)
+    assert(Seq(0, 1, 2) === itemForm("tags").indexes)
   }
 }
