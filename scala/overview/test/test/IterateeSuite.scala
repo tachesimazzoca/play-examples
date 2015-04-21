aca9eae22d784c84ef3e68bf47d7bc408d6e8529
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-04-21 17:19:48 +0900

    Add test (Done|Cont)Iteratee

--- scala/overview/test/test/IterateeSuite.scala
+++ scala/overview/test/test/IterateeSuite.scala
@@ -7,6 +7,7 @@ import play.api.libs.iteratee._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Success
 
 @RunWith(classOf[JUnitRunner])
 class IterateeSuite extends FunSuite {
@@ -17,19 +18,50 @@ class IterateeSuite extends FunSuite {
     assert(Input.EOF === Input.EOF.map(f))
   }
 
-  test("doneIteratee") {
-    def f(step: Step[String, Int]): Future[Option[Int]] = step match {
+  test("DoneIteratee") {
+    def folder(step: Step[String, Int]): Future[Option[Int]] = step match {
       case Step.Done(a, e) => Future(Some(a))
       case Step.Cont(k) => Future(None)
       case Step.Error(msg, e) => Future(None)
     }
 
-    val doneIteratee = new Iteratee[String, Int] {
+    val doneIt = new Iteratee[String, Int] {
       override def fold[B](folder: (Step[String, Int]) => Future[B])
                           (implicit ec: ExecutionContext): Future[B] =
         folder(Step.Done(1, Input.Empty))
     }
-    doneIteratee.fold(f).onComplete(a => println(a))
+    doneIt.fold(folder).onComplete(a => assert(Success(Some(1)) === a))
+
+    val doneIt2 = Done[String, Int](2, Input.Empty)
+    doneIt2.fold(folder).onComplete(a => assert(Success(Some(2)) === a))
+
+    Thread.sleep(100L)
+  }
+
+  test("ContIteratee") {
+    def folder(step: Step[String, Int]): Future[Int] = step match {
+      case Step.Done(a, e) => Future(a)
+      case Step.Cont(k) => k(Input.EOF).fold({
+        case Step.Done(a1, _) => Future.successful(a1)
+        case _ => throw new UnsupportedOperationException()
+      })
+      case _ => throw new UnsupportedOperationException()
+    }
+
+    val contIt = new Iteratee[String, Int] {
+      override def fold[B](folder: (Step[String, Int]) => Future[B])
+                          (implicit ec: ExecutionContext): Future[B] =
+        folder(Step.Cont {
+          case Input.El(e) => Done(e.toInt, Input.EOF)
+          case Input.Empty => this
+          case Input.EOF => Done(0, Input.EOF)
+        })
+    }
+    contIt.fold(folder).onComplete(a => assert(Success(0) === a))
+
+    val contIt2 = Cont[String, Int](in => Done(100, Input.Empty))
+    contIt2.fold(folder).onComplete(a => assert(Success(100) === a))
+
     Thread.sleep(100L)
   }
 }
