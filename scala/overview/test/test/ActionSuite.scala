5f84c8a1c779076d947dc5155a322268e1053b26
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-05-13 23:53:20 +0900

    Add tests with ActionBuilder

--- scala/overview/test/test/ActionSuite.scala
+++ scala/overview/test/test/ActionSuite.scala
@@ -4,28 +4,43 @@ import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.Logger
 import play.api.http.ContentTypes._
-import play.api.libs.iteratee.Iteratee
+import play.api.http.Status
+import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.mvc.BodyParsers.parse
 import play.api.mvc.Results._
 import play.api.mvc._
-import play.api.test.FakeRequest
+import play.api.test.{FakeApplication, FakeRequest}
 
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @RunWith(classOf[JUnitRunner])
-class ActionSuite extends FunSuite with ScalaFutures {
+class ActionSuite extends FunSuite with ScalaFutures with OneAppPerSuite {
 
-  class LoggingAction(buffer: ArrayBuffer[Any]) extends ActionBuilder[Request] {
-    override def invokeBlock[A](request: Request[A],
-                                block: (Request[A]) => Future[Result]): Future[Result] = {
-      buffer.append(request)
-      block(request)
+  implicit override lazy val app = FakeApplication()
+
+  private case class Logging[A](action: Action[A]) extends Action[A] {
+    override def apply(request: Request[A]): Future[Result] = {
+      Logger.info(request.toString)
+      action(request)
     }
+
+    lazy val parser = action.parser
   }
 
-  val bytesConsumer = Iteratee.consume[Array[Byte]]()
+  private object LoggingAction extends ActionBuilder[Request] {
+    override def invokeBlock[A](request: Request[A],
+                                block: (Request[A]) => Future[Result]): Future[Result] = {
+      block(request)
+    }
+
+    override protected def composeAction[A](action: Action[A]): Action[A] = new Logging(action)
+  }
+
+  private val bytesConsumer = Iteratee.consume[Array[Byte]]()
 
   test("apply") {
     val body = "Hello"
@@ -41,16 +56,49 @@ class ActionSuite extends FunSuite with ScalaFutures {
     }
   }
 
-  test("LoggingAction") {
-    val buffer = new ArrayBuffer[Any]()
+  test("Logging") {
+    val req = FakeRequest("POST", "/logging")
 
-    val action = new LoggingAction(buffer).apply {
+    val action = Logging {
+      Action {
+        Ok("")
+      }
+    }
+    val xmlAction = Logging {
+      Action(parse.tolerantXml) { request =>
+        Ok("")
+      }
+    }
+
+    whenReady(action(req)) { r =>
+      assert(Status.OK === r.header.status)
+    }
+    whenReady(Enumerator("<foo>bar</foo>".getBytes()) |>>> xmlAction(req)) { r =>
+      assert(Status.OK === r.header.status)
+    }
+    whenReady(Enumerator("".getBytes()) |>>> xmlAction(req)) { r =>
+      assert(Status.BAD_REQUEST === r.header.status)
+    }
+  }
+
+  test("LoggingAction") {
+    val req = FakeRequest("POST", "/loggingAction")
+
+    val action = LoggingAction {
+      Ok("")
+    }
+    val xmlAction = LoggingAction(parse.tolerantXml) { request =>
       Ok("")
     }
 
-    val req = FakeRequest("GET", "/foo")
-    whenReady(action(req)) { _ =>
-      assert(req === buffer(0))
+    whenReady(action(req)) { r =>
+      assert(Status.OK === r.header.status)
+    }
+    whenReady(Enumerator("<foo>bar</foo>".getBytes()) |>>> xmlAction(req)) { r =>
+      assert(Status.OK === r.header.status)
+    }
+    whenReady(Enumerator("".getBytes()) |>>> xmlAction(req)) { r =>
+      assert(Status.BAD_REQUEST === r.header.status)
     }
   }
 }
