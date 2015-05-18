4c52f8d9df9513e65877f0439f6ffb5c0ac38f58
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-05-18 17:48:09 +0900

    Add test GzipFilter

--- scala/overview/project/Build.scala
+++ scala/overview/project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    filters,
     "org.scalatestplus" % "play_2.10" % "1.0.0" % "test"
   )
 
--- scala/overview/test/test/FilterSuite.scala
+++ scala/overview/test/test/FilterSuite.scala
@@ -5,10 +5,12 @@ import org.scalatest.FunSuite
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.junit.JUnitRunner
 import org.scalatestplus.play.OneAppPerSuite
+import play.api.http.HeaderNames._
 import play.api.libs.iteratee.Iteratee
 import play.api.mvc.Results._
 import play.api.mvc._
-import play.api.test.FakeRequest
+import play.api.test._
+import play.filters.gzip._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -50,6 +52,31 @@ class FilterSuite extends FunSuite with ScalaFutures with OneAppPerSuite {
     } yield (result, byteToStr(bytes))) { t =>
       assert(t._1.header.headers.get("Set-Cookie").isEmpty)
       assert("sessionId: deadbeef" === t._2)
+    }
+  }
+
+  test("GzipFilter") {
+    val filter = new GzipFilter(512)
+    val body = "a" * 100
+    val action = Action {
+      Ok(body)
+    }
+
+    val hlen = 10
+    val expected = Array(
+      0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x4b, 0x4c, 0xa4, 0x3d,
+      0x00, 0x00, 0x64, 0x7a,
+      0x70, 0xaf, 0x64, 0x00,
+      0x00, 0x00).map(_.toByte)
+
+    val req = FakeRequest().withHeaders(ACCEPT_ENCODING -> "gzip")
+    whenReady(for {
+      result <- filter(action)(req).run
+      bytes <- result.body |>>> bytesConsumer
+    } yield (result, bytes)) { t =>
+      assert(Some("gzip") === t._1.header.headers.get(CONTENT_ENCODING))
+      assert(expected === t._2)
     }
   }
 }
