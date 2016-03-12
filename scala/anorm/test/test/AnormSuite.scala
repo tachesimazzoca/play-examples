5037e69ae9ecaa543be21b3098258d008e1910b6
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2016-03-13 02:06:11 +0900

    Use loan pattern withUserTable

--- scala/anorm/test/test/AnormSuite.scala
+++ scala/anorm/test/test/AnormSuite.scala
@@ -1,5 +1,7 @@
 package test
 
+import java.sql.Connection
+
 import anorm._
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
@@ -10,9 +12,10 @@ import play.api.db._
 @RunWith(classOf[JUnitRunner])
 class AnormSuite extends FunSuite with OneAppPerSuite {
 
-  test("NamedParameter") {
-    Databases.withInMemory() { database =>
+  case class User(id: Int, email: String, birthday: java.util.Date)
 
+  private def withUserTable(rows: Seq[User])(block: Connection => Any) {
+    Databases.withInMemory() { database =>
       implicit val conn = database.getConnection()
 
       SQL("DROP TABLE IF EXISTS users").executeUpdate()
@@ -23,47 +26,51 @@ class AnormSuite extends FunSuite with OneAppPerSuite {
              |  birthday datetime
              |)""".stripMargin).executeUpdate()
 
-      val timestamp: (Long) => Long = System.currentTimeMillis + _ / 1000 * 1000
+      val insertQuery = SQL(
+        """INSERT INTO users VALUES ({id}, {email}, {birthday})"""
+      )
+      rows.foreach { row =>
+        val params = Seq[NamedParameter](
+          'id -> row.id,
+          'email -> row.email,
+          'birthday -> row.birthday)
+        insertQuery.on(params: _*).executeUpdate()
+      }
 
-      val insertQuery = SQL( """INSERT INTO users VALUES ({id}, {email}, {birthday})""")
-
-      val birthday1 = new java.util.Date(timestamp(0))
-      val params1 = Seq[NamedParameter](
-        'id -> 1,
-        'email -> "user1@example.net",
-        'birthday -> birthday1)
-      insertQuery.on(params1: _*).executeUpdate()
-
-      val birthday2 = new java.util.Date(timestamp(86400 * 1000))
-      val values = Seq[ParameterValue](2, "user2@example.net", birthday2)
-      val params2 = Seq[NamedParameter](
-        'id -> values(0),
-        'email -> values(1),
-        'birthday -> values(2))
-      insertQuery.on(params2: _*).executeUpdate()
-
-      val selectQuery = SQL( """SELECT * FROM users WHERE id = {id}""")
-
-      case class User(id: Int, email: String, birthday: java.util.Date)
-
-      val parser =
-        SqlParser.int("id") ~
-          SqlParser.str("email") ~
-          SqlParser.date("birthday") map {
-          case id ~ email ~ birthday => User(id, email, birthday)
-        }
-
-      val row1 = selectQuery.on('id -> 1).as(parser.single)
-      assert(row1.id === 1)
-      assert(row1.email === "user1@example.net")
-      assert(row1.birthday.getTime() === birthday1.getTime())
-
-      val row2 = selectQuery.on('id -> 2).as(parser.single)
-      assert(row2.id === 2)
-      assert(row2.email === "user2@example.net")
-      assert(row2.birthday.getTime() === birthday2.getTime())
+      block(conn)
 
       SQL("DROP TABLE users").executeUpdate()
+    }
+  }
+
+  private def timestamp(x: Long = 0): Long = {
+    System.currentTimeMillis + x / 1000 * 1000
+  }
+
+  test("SqlParser") {
+    val rows = Seq(
+      User(1, "user1@example.net", new java.util.Date(timestamp())),
+      User(2, "user2@example.net", new java.util.Date(timestamp(86400 * 1000)))
+    )
+
+    withUserTable(rows) { implicit conn =>
+      val selectQuery = SQL( """SELECT * FROM users WHERE id = {id}""")
+
+      val parser = SqlParser.int("id") ~
+        SqlParser.str("email") ~
+        SqlParser.date("birthday") map {
+        case id ~ email ~ birthday => User(id, email, new java.util.Date(birthday.getTime))
+      }
+
+      val row1 = selectQuery.on('id -> 1).as(parser.single)
+      assert(row1.id === rows(0).id)
+      assert(row1.email === rows(0).email)
+      assert(row1.birthday === rows(0).birthday)
+
+      val row2 = selectQuery.on('id -> 2).as(parser.single)
+      assert(row2.id === rows(1).id)
+      assert(row2.email === rows(1).email)
+      assert(row2.birthday === rows(1).birthday)
     }
   }
 }
