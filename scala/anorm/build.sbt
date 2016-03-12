36d472dce5e987dc574f2bd41e2b0f9f2a3ee9b9
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2016-03-12 19:14:10 +0900

    Migrate from 2.3 to 2.5

---
+++ scala/anorm/build.sbt
@@ -0,0 +1,16 @@
+name := "play-anorm"
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.11.8"
+
+javaOptions in Test += "-Dconfig.file=conf/application.test.conf"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  "com.typesafe.play" %% "anorm" % "2.5.0",
+  "com.h2database" % "h2" % "1.4.178",
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % "test"
+)
+
+lazy val main = (project in file(".")).enablePlugins(PlayScala)
--- scala/anorm/test/test/AnormSuite.scala
+++ scala/anorm/test/test/AnormSuite.scala
@@ -1,35 +1,31 @@
 package test
 
-import org.scalatest.FunSuite
-
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-
-import org.scalatestplus.play.OneAppPerSuite
-
-import play.api.test._
-import play.api.db._
-
 import anorm._
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.db._
 
 @RunWith(classOf[JUnitRunner])
 class AnormSuite extends FunSuite with OneAppPerSuite {
 
-  implicit override lazy val app = FakeApplication()
-
   test("NamedParameter") {
-    DB.withConnection { implicit conn =>
+    Databases.withInMemory() { database =>
+
+      implicit val conn = database.getConnection()
+
       SQL("DROP TABLE IF EXISTS users").executeUpdate()
-      SQL("""
-        |CREATE TABLE users (
-        |  id int(11),
-        |  email varchar(255) NOT NULL default '',
-        |  birthday datetime
-        |)""".stripMargin).executeUpdate()
+      SQL( """
+             |CREATE TABLE users (
+             |  id int(11),
+             |  email varchar(255) NOT NULL default '',
+             |  birthday datetime
+             |)""".stripMargin).executeUpdate()
 
       val timestamp: (Long) => Long = System.currentTimeMillis + _ / 1000 * 1000
 
-      val insertQuery = SQL("""INSERT INTO users VALUES ({id}, {email}, {birthday})""")
+      val insertQuery = SQL( """INSERT INTO users VALUES ({id}, {email}, {birthday})""")
 
       val birthday1 = new java.util.Date(timestamp(0))
       val params1 = Seq[NamedParameter](
@@ -46,15 +42,26 @@ class AnormSuite extends FunSuite with OneAppPerSuite {
         'birthday -> values(2))
       insertQuery.on(params2: _*).executeUpdate()
 
-      val selectQuery = SQL("""SELECT * FROM users WHERE id = {id}""")
-      val row1 = selectQuery.on('id -> 1).apply().head
-      assert(row1[Int]("id") === 1)
-      assert(row1[String]("email") === "user1@example.net")
-      assert(row1[java.util.Date]("birthday").getTime() === birthday1.getTime())
-      val row2 = selectQuery.on('id -> 2).apply().head
-      assert(row2[Int]("id") === 2)
-      assert(row2[String]("email") === "user2@example.net")
-      assert(row2[java.util.Date]("birthday").getTime() === birthday2.getTime())
+      val selectQuery = SQL( """SELECT * FROM users WHERE id = {id}""")
+
+      case class User(id: Int, email: String, birthday: java.util.Date)
+
+      val parser =
+        SqlParser.int("id") ~
+          SqlParser.str("email") ~
+          SqlParser.date("birthday") map {
+          case id ~ email ~ birthday => User(id, email, birthday)
+        }
+
+      val row1 = selectQuery.on('id -> 1).as(parser.single)
+      assert(row1.id === 1)
+      assert(row1.email === "user1@example.net")
+      assert(row1.birthday.getTime() === birthday1.getTime())
+
+      val row2 = selectQuery.on('id -> 2).as(parser.single)
+      assert(row2.id === 2)
+      assert(row2.email === "user2@example.net")
+      assert(row2.birthday.getTime() === birthday2.getTime())
 
       SQL("DROP TABLE users").executeUpdate()
     }
